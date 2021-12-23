da24d543c637e80d20c230526397a2f6568f860b
vnyakutya <victorn@hollywoodbets.net>
2021-12-23 15:10:45 +0200

    edited the main.tf file

--- main.tf
+++ main.tf
@@ -13,12 +13,12 @@ provider "github" {
 }
 
 
-data "github_repository" "main" {
+data "github_repository" "test" {
   full_name = "vnyakutya/terraform_prac"
 }
 
 resource "github_branch_protection" "main" {
-  repository_id     = data.github_repository.main.node_id
+  repository_id    = data.github_repository.test.node_id
   pattern          = "main"
   enforce_admins   = true
   allows_deletions = true
@@ -32,3 +32,17 @@ output "debug-data" {
 output "debug-branch-protection" {
   value = github_branch_protection.main
 }
+
+data "github_user" "current" {
+  username = ""
+}
+
+output "current_github_login" {
+  value = "${data.github_user.current.login}"
+}
+
+required_pull_request_reviews {
+    dismiss_stale_reviews  = true
+    restrict_dismissals    = true
+    dismissal_restrictions = data.github_user.current.node_id
+  }
