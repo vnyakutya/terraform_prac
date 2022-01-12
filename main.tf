6bcb7c4a36a1fb48651f21e88f9882597e08422b
vnyakutya <victorn@hollywoodbets.net>
2022-01-12 11:27:44 +0200

    Added a new file: outputs.tf

--- main.tf
+++ main.tf
@@ -34,11 +34,3 @@ resource "github_branch_protection" "main" {
     required_approving_review_count = 1
   } */
 } 
-
-output "debug-data" {
-  value = data.github_repository.test
-}
-
-output "debug-branch-protection" {
-  value = github_branch_protection.main
-}
